3007b241c825d95b62cf249c2e4710b5210d0c8d
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2023-09-24 17:37:45 -0300

    Criando o Botão.

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,37 @@ main() {
   runApp(new PerguntaApp());
 }
 
-@override
 class PerguntaApp extends StatelessWidget {
+  final List<String> perguntas = ['Qual é o problema?', 'Qual é a sala?'];
+
+  void responder() {
+    print('Pergunta Respondida');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Perguntas'),
-        ),
-        body: Text('Olá Mundo Mobile!'),
-      ),
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: Column(
+            children: [
+              Text(perguntas[0]),
+              ElevatedButton(
+                child: Text('Resposta 1'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 2'),
+                onPressed: responder,
+              ),
+              ElevatedButton(
+                child: Text('Resposta 3'),
+                onPressed: responder,
+              )
+            ],
+          )),
     );
   }
 }
